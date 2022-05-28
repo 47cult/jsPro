04b917101e29724b2138c3a46f05480497c22798
47cult <96499405+47cult@users.noreply.github.com>
2022-05-28 19:21:44 +0300

    Update main.js

--- lesson 1/script/main.js
+++ lesson 1/script/main.js
@@ -1,24 +1,61 @@
-const goods = [{
-        title: 'Shirt',
-        price: 150
-    },
-    {
-        title: 'Socks',
-        price: 50
-    },
-    {
-        title: 'Jacket',
-        price: 350
-    },
-    {
-        title: 'Shoes',
-        price: 250
-    },
-];
+    const goods = [{
+            title: 'Shirt',
+            price: 150
+        },
+        {
+            title: 'Socks',
+            price: 50
+        },
+        {
+            title: 'Jacket',
+            price: 350
+        },
+        {
+            title: 'Shoes',
+            price: 250
+        },
+    ];
 
-const renderGoodsItem = (title = 'product', price = 100) => `<div class="goods-item"><div class="catalog__item-img">product</div><h3>${title}</h3><p>${price}</p></div>`;
-const renderGoodsList = list => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price)).join('');
-    document.querySelector('.catalog__products-list').innerHTML = goodsList;
-}
-renderGoodsList(goods);
+    class GoodsItem {
+        constructor({
+            title,
+            price
+        }) {
+            this.title = title;
+            this.price = price;
+        }
+        render() {
+            return `
+            <div class="goods-item">
+                <div class="catalog__item-img">product</div>
+                <h3>${this.title}</h3><p>${this.price}</p>
+            </div>`;
+        }
+    }
+
+    class GoodsList {
+        items = [];
+        fetchGoods() {
+            this.items = goods;
+        }
+        render() {
+            const goods = this.items.map(item => {
+                const goodItem = new GoodsItem(item);
+                return goodItem.render()
+            }).join('');
+
+            document.querySelector('.catalog__products-list').innerHTML = goods;
+        }
+        calculate() {
+            return this.items.reduce((prev, {
+                price
+            }) => {
+                return prev + price
+            }, 0)
+        }
+    }
+
+    const list = new GoodsList();
+    list.fetchGoods();
+    list.render();
+    console.log(list.calculate());
