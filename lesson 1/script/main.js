9747664f0544cc3d5948716f0c960968b9323269
47cult <96499405+47cult@users.noreply.github.com>
2022-06-14 19:53:35 +0300

    lesson6

--- lesson 1/script/main.js
+++ lesson 1/script/main.js
@@ -5,7 +5,80 @@
         return fetch(url).then((response => response.json()))
     }
 
+
+
     function init() {
+
+        const headerButton = Vue.component('custom-button', {
+            template: `
+                <button class = "header__button" type = "button" @click="$emit('click')">
+                    <slot></slot>
+                </button>
+            `
+        })
+
+        const basketGoods = Vue.component('basket-goods', {
+            template: `
+                <div class="fixed-area">
+                    <div class="basket-card">
+                        <div class="basket-card__header">
+                            <h1 class="basket-card__header__title">basket card</h1>
+                            <div class="basket-card__header__delete-icon" @click="$emit('click')"></div>
+                        </div>
+                        <div class="basket-card__content">
+                        <slot></slot>
+                        </div>
+                    </div>
+                </div>
+                `
+        })
+
+        const basketItems = Vue.component('basket-items', {
+            props: [
+                'item'
+            ],
+            template: `
+            <ul class="basket__list">
+            <li class="basket__item">
+                <h2>{{ item.product_name }}</h2>
+                <h3>{{ item.price }}</h3>
+                <p>{{ item.quantity }}</p>
+
+            </li>
+            </ul>
+            `
+        })
+
+        const goodItem = Vue.component('good-item', {
+            props: [
+                'item'
+            ],
+            template: `
+            <div class="catalog__item">
+                <div class="catalog__item-img">product</div>
+                <h3>{{ item.product_name }}</h3>
+                <p>{{ item.price }}</p>
+            </div>
+            `
+        })
+
+        const searchInput = Vue.component('search-input', {
+            model: {
+                prop: 'value',
+                event: 'input'
+            },
+            props: {
+                value: String
+            },
+            template: `
+            <input type="text"
+                    class="header__search"
+                    :value="value"
+                    @input="$emit('input', $event.target.value)"
+            />
+            `
+        })
+
         const app = new Vue({
             el: '#root',
             data: {
